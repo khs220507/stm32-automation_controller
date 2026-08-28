3809ce246f68c40b51eabaa4603f4fde7174f91d
khs220507 <khs220507@gmail.com>
2026-08-28 22:45:56 +0900

    USART2 클록 초기화 기록

--- firmware/automation_controller/Src/main.c
+++ firmware/automation_controller/Src/main.c
@@ -42,6 +42,18 @@ typedef enum
         GPIOA->BSRR = GPIO_BSRR_BR5;
     }
 
+  static void usart2_pins_init(void)
+    {
+        RCC->APB1ENR |= RCC_APB1ENR_USART2EN;
+
+        GPIOA->MODER &= ~(GPIO_MODER_MODER2 | GPIO_MODER_MODER3);
+        GPIOA->MODER |= GPIO_MODER_MODER2_1 | GPIO_MODER_MODER3_1;
+
+        GPIOA->AFR[0] &= ~(GPIO_AFRL_AFRL2 | GPIO_AFRL_AFRL3);
+        GPIOA->AFR[0] |= (7U << GPIO_AFRL_AFRL2_Pos);
+        GPIOA->AFR[0] |= (7U << GPIO_AFRL_AFRL3_Pos);
+    }
+
   int main(void)
   {
 	  while (1)
@@ -50,6 +62,7 @@ typedef enum
 		  {
 		  case STATE_INIT:
 			  safe_output_init();
+			  usart2_pins_init();
 			  current_state = STATE_IDLE;
 			  break;
 
